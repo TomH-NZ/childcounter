d10c5c5f31fc8f533ae289526983166bcf3661e8
Tom Hatcher <39353602+TomH-NZ@users.noreply.github.com>
2025-05-27 18:21:03 +1200

    Add basic logic to remove child from list.

--- Program.cs
+++ Program.cs
@@ -6,12 +6,30 @@ using childcounter;
 
 
 var data = new Api().GetChildDataAsync();
+var elimCount = data.Eliminate;
+var childTotal = data.ChildCount;
 
-List<string> childList = new List<string>();
-int num;
+// Possibly extract out to separate class and feed in data from api??
+var childList = new List<string>();
+var eliminatedList = new List<string>();
+int count;
 
-for (num = 1; num <= data.ChildCount; num++)
+for (count = 1; count <= childTotal;)
 {
-    childList.Add($"Child {num}");
+    childList.Add($"Child {count}");
+    count++;
 }
 
+while (childList.Count > 1)
+{
+    if ((data.Eliminate % data.ChildCount) > 0)
+    {
+        elimCount = data.Eliminate - data.ChildCount;
+    }
+    
+    eliminatedList.Add(childList[elimCount]);
+    childList.RemoveAt(elimCount);
+}
+
+Console.WriteLine($"The game is {data.Id}");
+Console.WriteLine($"The surviving child is {childList[0]}");
